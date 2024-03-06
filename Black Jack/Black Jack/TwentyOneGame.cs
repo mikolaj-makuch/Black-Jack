19330ad07da234a474b11665d4712f9ae3cc4b7b
micuch11 <micuch1126@gmail.com>
2024-03-06 05:51:48 +0000

    Update

---
+++ Black Jack/Black Jack/TwentyOneGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public class TwentyOneGame : Game
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
